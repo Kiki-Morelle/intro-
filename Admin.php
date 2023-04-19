4837dbe8ef76dd92e69b6896a17c590f87042626
OBEN HELEN NSO <123206011+Obenhelen@users.noreply.github.com>
2023-04-18 21:59:03 -0700

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -3,7 +3,10 @@ include "conn.php";
 if(isset($_POST['email'])){
     $email = $_POST['email'];
 
-    $sql= "INSERT INTO user(email) VALUES(?)";
-    $stmt = $con->prepare($sql);
-    $stmt->execute([$email]);
+$sql = "INSERT INTO user(email) VALUES('$email')";
+        // echo " '$email'";
+    mysqli_query($con,$sql);
+}
+else{
+    echo "value email has not been set";
 }
